ad2c19a130f6cbafb9213d7ea2282f02dcdda9b7
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-06-06 20:01:06 +0300

    add assets

---
+++ Assets/Downloads/Particles/Epic Toon FX/Demo/Scripts/VFX Library/PEButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum ButtonTypes {
+	NotDefined,
+	Previous,
+	Next
+}
+
+public class PEButtonScript : MonoBehaviour, IEventSystemHandler, IPointerEnterHandler, IPointerExitHandler {
+	#pragma warning disable 414
+	//private AbstractButton myButton;
+	#pragma warning disable 414
+	public ButtonTypes ButtonType = ButtonTypes.NotDefined;
+
+	// Use this for initialization
+	void Start () {
+		//myButton = gameObject.GetComponent<AbstractButton> ();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		// Used for Tooltip
+		UICanvasManager.GlobalAccess.MouseOverButton = true;
+		UICanvasManager.GlobalAccess.UpdateToolTip (ButtonType);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		// Used for Tooltip
+		UICanvasManager.GlobalAccess.MouseOverButton = false;
+		UICanvasManager.GlobalAccess.ClearToolTip ();
+	}
+
+	public void OnButtonClicked () {
+		// Button Click Actions
+		UICanvasManager.GlobalAccess.UIButtonClick(ButtonType);
+	}
+}

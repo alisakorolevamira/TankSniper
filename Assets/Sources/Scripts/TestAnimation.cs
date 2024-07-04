0e921137cb0974629f648d9668294d915eea921a
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-07-04 08:20:06 +0300

    added robot

---
+++ Assets/Sources/Scripts/TestAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sources.Scripts
+{
+    public class TestAnimation : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameObject;
+        [SerializeField] private Collider _collider;
+        [SerializeField] private Button _button;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(RemoveMesh);
+        }
+
+        private void RemoveMesh()
+        {
+            _gameObject.SetActive(true);
+            _collider.SendMessage("Shatter", transform.position, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}

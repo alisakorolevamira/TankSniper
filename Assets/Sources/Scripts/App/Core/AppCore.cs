c90a9558e23da7b936ff4f04b1eca86a3d2c0856
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-07-07 13:24:15 +0500

    changed camera

--- Assets/Sources/Scripts/App/Core/AppCore.cs
+++ Assets/Sources/Scripts/App/Core/AppCore.cs
@@ -20,8 +20,8 @@ namespace Sources.Scripts.App.Core
             {
                 await _sceneService.ChangeSceneAsync(
                     //ModelId.MainMenu,
-                    LevelConst.ThirdLevel,
-                    new ScenePayload(LevelConst.ThirdLevel, false, false));
+                    LevelConst.EighthLevel,
+                    new ScenePayload(LevelConst.EighthLevel, false, false));
             }
             catch(ArgumentNullException)
             {

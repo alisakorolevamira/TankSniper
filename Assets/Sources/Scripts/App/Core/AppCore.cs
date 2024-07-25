a12731af2ad59aff225153dce96e2e07ab3d345c
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-07-25 20:38:25 +0500

    changed levels

--- Assets/Sources/Scripts/App/Core/AppCore.cs
+++ Assets/Sources/Scripts/App/Core/AppCore.cs
@@ -20,7 +20,7 @@ namespace Sources.Scripts.App.Core
             {
                 await _sceneService.ChangeSceneAsync(
                     ModelId.MainMenu,
-                    //LevelConst.EleventhLevel,
+                    //LevelConst.SeventhLevel,
                     new ScenePayload(ModelId.MainMenu, false, false));
             }
             catch(ArgumentNullException)
